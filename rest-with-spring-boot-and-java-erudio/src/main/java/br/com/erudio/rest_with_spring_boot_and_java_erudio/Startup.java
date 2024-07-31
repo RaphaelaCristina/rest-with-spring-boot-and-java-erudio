d313928fcb84c804b57415371398e5847db89bc4
RaphaelaCristina <raphitasp@msn.com>
2024-07-31 11:34:37 -0300

    renomeando classe e apagando pastas

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/rest_with_spring_boot_and_java_erudio/Startup.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/rest_with_spring_boot_and_java_erudio/Startup.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestWithSpringBootAndJavaErudioApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestWithSpringBootAndJavaErudioApplication.class, args);
-		
+		SpringApplication.run(Startup.class, args);
+
 	}
 
 }
